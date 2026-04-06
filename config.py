1fed26b5c311ac1a486a1b2fb5ca99b7a94c5a25
ytsuperstar2025-dot <ytsuperstar2025@gmail.com>
2026-04-06 15:39:52 +0530

    Refactor TOKEN and ADMIN_ID to use environment variables

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN = "8758907279:AAFRs5fAP_JY92quH5K8xyi-brVUpDk_pAw"
-ADMIN_ID = 5572607088
+import os
+
+TOKEN = os.getenv("8758907279:AAFRs5fAP_JY92quH5K8xyi-brVUpDk_pAw")
+ADMIN_ID = int(os.getenv("5572607088"))
