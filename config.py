ea4e1630e86cd39e0fd7f5195689c2648c3605e5
ytsuperstar2025-dot <ytsuperstar2025@gmail.com>
2026-04-06 16:43:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
 
 TOKEN = os.getenv("TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+if TOKEN is None:
+    raise ValueError("TOKEN environment variable missing!")
+
+if ADMIN_ID is None:
+    raise ValueError("ADMIN_ID environment variable missing!")
+
+ADMIN_ID = int(ADMIN_ID)
